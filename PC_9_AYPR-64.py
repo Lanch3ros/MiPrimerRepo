a7ed1e996b333c913abad312bd703ecd574a9916
Lanch3ros <ayora913@gmail.com>
2024-10-22 09:08:56 -0500

    Finish PC_9

--- PC_9_AYPR-64.py
+++ PC_9_AYPR-64.py
@@ -2,7 +2,7 @@
 Universidad Escuela Colombiana de Ingeniería
 Profesora: Ingeniera Patricia Salazar Perdomo
 
-Estudiante: Apellidos Nombre
+Estudiante: Lancheros Ayora José Luis
 
 Lenguaje: Python
 Referencia: Prueba corta No. 9. Programación modular con vectores.
@@ -25,13 +25,20 @@ b. Construya una función en Python que averigüe la cantidad de asignaturas con
    no debe escribirlo en ella.
    Escriba un ejemplo de invocación (tenga en cuenta los nombres de las
    variables que se dan).  
-
 '''
 
 print ("\nPARTE a.")
 
 # Definición de la función.
+def calc_mayor_calif_y_nom (nombre, calif_p, materias):
+   calif_mayor = 0
+   materia_mayor = " "
+   for i in range (materias):
+      if calif_p[i] > calif_mayor:
+         calif_mayor = calif_p[i]
+         materia_mayor = nombre[i]
 
+   return calif_mayor, materia_mayor
 
 # Datos de prueba.
 nom_as = ["CALD", "CIPP", "AYPR", "FIEM", "HGCL"]
@@ -39,18 +46,24 @@ calif_prom = [3.1, 4.2, 4.1, 4.0, 2.8]
 cant_as = 5
 
 # Ejemplo de invocación.
-
+calif_mayor, materia_mayor = calc_mayor_calif_y_nom (nom_as, calif_prom, cant_as)
 
 # Verificación del resultado.
-
+print (f"La materia con la califficacion promedio mas alta es {materia_mayor} y es {calif_mayor}.")
 
 print ("\nPARTE b.")
 
 # Definición de la función.
-
+def materias_en_intervalo (calif, materias, calif_mayor):
+   ct_as = 0
+   for i in range (materias):
+      if calif[i] >= 3 and calif[i] <= calif_mayor:
+         ct_as += 1
+   return ct_as
 
 # Ejemplo de invocación.
 
+ct_as = materias_en_intervalo (calif_prom, cant_as, calif_mayor)
 
 # Verificación del resultado.
-
+print (f"Hay {ct_as} de materias que se encuentran en el intervalo [3, {calif_mayor}].\n")
