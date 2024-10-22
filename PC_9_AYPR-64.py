6e9d661fead2bf64785f6a09f899427902a09ee7
Lanch3ros <ayora913@gmail.com>
2024-10-22 08:47:18 -0500

    Added PC_9

---
+++ PC_9_AYPR-64.py
@@ -0,0 +1,56 @@
+'''
+Universidad Escuela Colombiana de Ingeniería
+Profesora: Ingeniera Patricia Salazar Perdomo
+
+Estudiante: Apellidos Nombre
+
+Lenguaje: Python
+Referencia: Prueba corta No. 9. Programación modular con vectores.
+
+Se tienen dos vectores. uno con los nombres de las asignaturas (nom_as) de
+cierto plan de estudios y otro con la calificación promedio obtenida
+(calif_prom). Los vectores y su tamaño (cant_as) ya se conocen. 
+
+a. Construya una función en Python que averigüe el nombre de la asignatura con
+   la menor calificación promedio y cuál fue. Los resultados deben quedar
+   disponibles donde se invoque la función. Es decir, no debe escribirlos en
+   ella.
+   Escriba un ejemplo de invocación (tenga en cuenta los nombres de las
+   variables que se dan).  
+
+b. Construya una función en Python que averigüe la cantidad de asignaturas con
+   calificación promedio que pertenezca al intervalo [3.0, mayor calificación
+   promedio (uno de los resultados de la parte a que no debe averiguar de nuevo)].
+   El resultado debe quedar disponible donde se invoque la función. Es decir,
+   no debe escribirlo en ella.
+   Escriba un ejemplo de invocación (tenga en cuenta los nombres de las
+   variables que se dan).  
+
+'''
+
+print ("\nPARTE a.")
+
+# Definición de la función.
+
+
+# Datos de prueba.
+nom_as = ["CALD", "CIPP", "AYPR", "FIEM", "HGCL"]
+calif_prom = [3.1, 4.2, 4.1, 4.0, 2.8]
+cant_as = 5
+
+# Ejemplo de invocación.
+
+
+# Verificación del resultado.
+
+
+print ("\nPARTE b.")
+
+# Definición de la función.
+
+
+# Ejemplo de invocación.
+
+
+# Verificación del resultado.
+
